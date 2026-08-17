b70324bd8164d7122a5b4f994419e3eaf90c252f
ErnestMampana <ernest.mampana@gmail.com>
2026-08-17 21:49:42 +0200

    Project creation

---
+++ src/main/java/za/co/ernest/quickbite/QuickbiteApplication.java
@@ -0,0 +1,13 @@
+package za.co.ernest.quickbite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickbiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickbiteApplication.class, args);
+	}
+
+}
